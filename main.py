3c165ab4143be7f381221ea0fa2f4fabf7df2e66
IDGS-801-22001417 <82052@alumnos.utleon.edu.mx>
2025-02-17 18:21:37 -0600

    17-02-2025 ZodiacoChinoFlask

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from datetime import datetime
 
 app=Flask(__name__)
 
@@ -93,9 +94,95 @@ def resultado():
     except ValueError:
         return "Error: Por favor ingrese números válidos."
 
-@app.route("/cinepolis")
-def cine():
+@app.route("/Cinepolis")
+def Cinepolis():
     return render_template("Cinepolis.html")
+
+
+@app.route("/comprar", methods=["POST"])
+def comprar():
+    precio_entrada = 12.00
+    try:
+        personas = request.form.get("cantidadCompradores")
+        boletos = request.form.get("cantidadBoletas")
+        tarjeta = request.form.get("tarjetaCineco")
+
+        if not personas or not personas.isdigit() or int(personas) <= 0:
+            return render_template("Cinepolis.html", total="La cantidad de Compradores debe ser un número mayor a 0.")
+        
+        if not boletos or not boletos.isdigit() or int(boletos) <= 0:
+            return render_template("Cinepolis.html", total="La cantidad de Boletas debe ser un número mayor a 0.")
+
+        if tarjeta not in ["si", "no"]:
+            return render_template("Cinepolis.html", total="Debes seleccionar una opción válida para Tarjeta Cineco.")
+
+        personas = int(personas)
+        boletos = int(boletos)
+
+        if boletos > personas * 7:
+            return render_template("Cinepolis.html", total="No puedes comprar más de 7 boletos por persona.")
+
+        total = boletos * precio_entrada
+        if 3 <= boletos <= 5:
+            total *= (1 - 0.10)
+        elif boletos > 5:
+            total *= (1 - 0.15)
+        
+        if tarjeta == "si":
+            total *= (1 - 0.10)
+        
+        return render_template("Cinepolis.html", total = str(round(total, 2)))
+
+    except Exception as ex:
+        return render_template("Cinepolis.html", total=f"Error: {str(ex)}")
+
+@app.route("/ZodiacoChino")
+def Zodiaco():
+    return render_template("ZodiacoChino.html")
+
+def calcular_edad(dia, mes, anio):
+    hoy = datetime.now()
+    fecha_nacimiento = datetime(anio, mes, dia)
+    edad = hoy.year - fecha_nacimiento.year
+    if (hoy.month, hoy.day) < (fecha_nacimiento.month, fecha_nacimiento.day):
+        edad -= 1
+    return edad
+
+def signo_zodiacal_chino(anio):
+    signos = [
+        "Mono", "Gallo", "Perro", "Cerdo", "Rata", 
+        "Buey", "Tigre", "Conejo", "Dragon", "Serpiente", 
+        "Caballo", "Cabra"
+    ]
+    return signos[anio % 12]
+
+@app.route("/imprimir", methods=["POST"])
+def Imprimir():
+    nombre = request.form.get("nombre")
+    apaterno = request.form.get("apaterno")
+    amaterno = request.form.get("amaterno")
+    dia = int(request.form.get("dia"))
+    mes = int(request.form.get("mes"))
+    anio = int(request.form.get("anio"))
+    sexo = request.form.get("sexo")
+
+    edad = calcular_edad(dia, mes, anio)
+    signo = signo_zodiacal_chino(anio)
+
+    if sexo == "masculino":
+        sexo = "Hombre"
+    else:
+        sexo = "Mujer"
+    
+    return render_template(
+        "ZodiacoChino.html", 
+        nombre=nombre, 
+        apaterno=apaterno, 
+        amaterno=amaterno, 
+        edad=edad, 
+        signo=signo,
+        sexo=sexo
+    )
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
